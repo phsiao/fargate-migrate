dcfea3f5d7a6f8685f08d18199f040e136986f83
Shawn Hsiao <phsiao@tripadvisor.com>
2021-04-12 22:18:38 -0400

    Set task cpu/memory using aggregated container cpu/memory requests (#7)
    * adds -cover for test
    * prettify the generated python code

--- internal/cdk/python/task.go
+++ internal/cdk/python/task.go
@@ -2,9 +2,11 @@ package python
 
 import (
 	"bytes"
+	"fmt"
 	"regexp"
 	"text/template"
 
+	"github.com/phsiao/fargate-migrate/internal/fargate"
 	corev1 "k8s.io/api/core/v1"
 )
 
@@ -23,25 +25,39 @@ func NewTaskDefinitionStatementGenerator(containers []corev1.Container) *TaskDef
 }
 
 func (g TaskDefinitionStatementGenerator) Generate() (string, error) {
+	accumCPU := int(0)
+	accumMemory := int(0)
+
+	for _, container := range g.Containers {
+		accumCPU += int(container.Resources.Requests.Cpu().MilliValue())
+		accumMemory += int(container.Resources.Requests.Memory().MilliValue() / (1000 * 1024 * 1024))
+	}
+
+	configCPU, configMemory := fargate.MinCPUMemroyConfiguration(accumCPU, accumMemory)
+
 	tmpl, err := template.New("taskdefinition").Funcs(
 		template.FuncMap{
 			"python": func(input string) string {
 				re := regexp.MustCompile(`\n`)
 				return re.ReplaceAllString(input, "\\n")
 			},
+			"configCPU": func() string {
+				return fmt.Sprintf("%d", configCPU)
+			},
+			"configMemory": func() string {
+				return fmt.Sprintf("%d", configMemory)
+			},
 		}).Parse(`
         task_def = ecs.FargateTaskDefinition(self, "{{.Name}}",
-            cpu=256,
-            memory_limit_mib=512)
+            cpu={{configCPU}},
+            memory_limit_mib={{configMemory}})
 {{range .Containers}}
-        env = {}
-{{range .Env}}
+        env = {}{{range .Env}}
         env["{{.Name}}"] = "{{.Value|python}}"{{end}}
 
         task_def.add_container("{{.Name}}",
             image=ecs.ContainerImage.from_asset("docker/{{.Name}}"),
-            port_mappings=[
-{{range .Ports}}
+            port_mappings=[{{range .Ports}}
               ecs.PortMapping(container_port={{.ContainerPort}}),{{end}}
             ],
             environment=env,
---
+++ internal/fargate/cpu_memory.go
@@ -0,0 +1,83 @@
+// fargate only supports specific configuration of cpu and memory
+// so this module provides a function to find minimal matching configuration
+// for the given cpu and memory
+// https://docs.aws.amazon.com/AmazonECS/latest/developerguide/AWS_Fargate.html
+package fargate
+
+type cpuMemoryConfig struct {
+	cpuConfig     int
+	memoryConfigs []int
+}
+
+var (
+	supportedCPUMemory = []cpuMemoryConfig{
+		{
+			cpuConfig:     256,
+			memoryConfigs: []int{512, 1024, 2048}},
+	}
+)
+
+func init() {
+	var config cpuMemoryConfig
+
+	config = cpuMemoryConfig{
+		cpuConfig:     512,
+		memoryConfigs: []int{},
+	}
+	for i := 1024; i <= 4096; i += 1024 {
+		config.memoryConfigs = append(config.memoryConfigs, i)
+	}
+	supportedCPUMemory = append(supportedCPUMemory, config)
+
+	config = cpuMemoryConfig{
+		cpuConfig:     1024,
+		memoryConfigs: []int{},
+	}
+	for i := 2048; i <= 8192; i += 1024 {
+		config.memoryConfigs = append(config.memoryConfigs, i)
+	}
+	supportedCPUMemory = append(supportedCPUMemory, config)
+
+	config = cpuMemoryConfig{
+		cpuConfig:     2048,
+		memoryConfigs: []int{},
+	}
+	for i := 4096; i <= 16384; i += 1024 {
+		config.memoryConfigs = append(config.memoryConfigs, i)
+	}
+	supportedCPUMemory = append(supportedCPUMemory, config)
+
+	config = cpuMemoryConfig{
+		cpuConfig:     4096,
+		memoryConfigs: []int{},
+	}
+	for i := 8192; i <= 30720; i += 1024 {
+		config.memoryConfigs = append(config.memoryConfigs, i)
+	}
+	supportedCPUMemory = append(supportedCPUMemory, config)
+}
+
+func MinCPUMemroyConfiguration(cpu, memory int) (matchCPU, matchMemory int) {
+
+	for _, config := range supportedCPUMemory {
+		if cpu > config.cpuConfig {
+			continue
+		}
+		matchCPU = config.cpuConfig
+		for _, memoryConfig := range config.memoryConfigs {
+			if memory > memoryConfig {
+				continue
+			} else {
+				matchMemory = memoryConfig
+				return
+			}
+		}
+	}
+
+	// if we reach here that means there is not matching minimal config
+	maxConfig := supportedCPUMemory[len(supportedCPUMemory)-1]
+	matchCPU = maxConfig.cpuConfig
+	matchMemory = maxConfig.memoryConfigs[len(maxConfig.memoryConfigs)-1]
+
+	return
+}
